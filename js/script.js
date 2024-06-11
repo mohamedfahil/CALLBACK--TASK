d3e04730817a22dd449133d30dd3afd4811b5aa0
mohamedfahil <mfahil26@gmail.com>
2024-06-11 22:43:23 +0530

    callback function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+let eventelement=document.getElementById("btn")
+let eventfun=(event)=>
+    {
+let countdownElement=document.getElementById("countdown");
+const countdownNumbers=[10,9,8,7,6,5,4,3,2,1,"Happy Independence Day!"];
+function displayCountdown(index){
+  if(index<countdownNumbers.length){
+    countdownElement.innerText=countdownNumbers[index];
+    setTimeout(function(){
+      displayCountdown(index+1);
+    },1000);
+  }
+}
+displayCountdown(0);}
+
+eventelement.addEventListener("click",eventfun)
